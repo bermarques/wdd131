64cc8d77b2e5ba5d90f0b67ed551dd27b9dd2e62
Bernardo Marques <bernardohmarques@gmail.com>
2024-07-21 03:03:46 -0300

    dynamic services

--- scripts/project.js
+++ scripts/project.js
@@ -17,9 +17,63 @@ function closeMenu() {
   navMenu.classList.remove("active");
 }
 
+const services = [
+  {
+    title: "Administrative",
+    icon: "adm.png",
+  },
+  {
+    title: "Labor and Social Security",
+    icon: "administrativo.png",
+  },
+  {
+    title: "Banking",
+    icon: "bancario.png",
+  },
+  {
+    title: "Consumer",
+    icon: "consumo.png",
+  },
+  {
+    title: "Enterprise",
+    icon: "trabalhista.png",
+  },
+  {
+    title: "Tributary",
+    icon: "tributario.png",
+  },
+  {
+    title: "Patents",
+    icon: "marcaspatentes.png",
+  },
+  {
+    title: "Health",
+    icon: "saude.png",
+  },
+];
+const createService = (services) => {
+  const grid = document.querySelector(".services");
+  grid.innerHTML = "";
+
+  services.forEach((service) => {
+    let serviceDiv = document.createElement("div");
+    serviceDiv.className = "service";
+
+    let serviceImage = document.createElement("img");
+    serviceImage.src = `../images/${service.icon}`;
+    serviceImage.alt = `${service.title} icon`;
+    serviceImage.loading = "lazy";
+    serviceDiv.appendChild(serviceImage);
+
+    let title = document.createElement("p");
+    title.textContent = `${service.title}`;
+    serviceDiv.appendChild(title);
+
+    grid.appendChild(serviceDiv);
+  });
+};
 const today = new Date();
 const currentYear = document.querySelector("#currentyear");
-const lastModified = document.querySelector("#lastModified");
 
 currentYear.innerHTML = today.getFullYear();
-lastModified.innerHTML = document.lastModified;
+createService(services);
